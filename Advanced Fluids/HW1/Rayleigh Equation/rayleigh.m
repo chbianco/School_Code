49d3be077fd6e57238f13fd34ccc8bb49ada852d
chbianco <christopherhbianco@gmail.com>
2025-02-09 13:59:12 -0500

    Adding sech profile and graphs

--- Advanced Fluids/HW1/Rayleigh Equation/rayleigh.m
+++ Advanced Fluids/HW1/Rayleigh Equation/rayleigh.m
@@ -2,7 +2,12 @@ function yp = rayleigh(y,f)
 
 global alpha c U Upp
 
-U = tanh(y);
-Upp = -2*tanh(y)*sech(y)^2;
+%Solve for tanh profile
+% U = tanh(y);
+% Upp = -2*tanh(y)*sech(y)^2;
+
+%Solve for sech profile
+U = sech(y);
+Upp = sech(y)*tanh(y)^2 - sech(y)^3;
 
 yp = [f(2); f(1)*(Upp/(U-c) + alpha^2) ];
